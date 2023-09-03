66bebb480aba576e4c77fd150f804e345e338751
r0pmy <143921691+r0pmy@users.noreply.github.com>
2023-09-03 11:57:54 +0700

    Update get_recommended_whitelist.sh

--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -32,7 +32,7 @@ urls=(
     https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
     # Commented out because it whitelists sites including doubleclick.net and ad.atdmt.com
     # https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt
-
+    https://raw.githubusercontent.com/nextdns/click-tracking-domains/main/domains
 )
 
 # loop through the urls and download each file with curl
